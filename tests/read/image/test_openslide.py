241222d49251b039d3a7858de07c8576fd3b9013
Lucas Diedrich <diedrich@biochem.mpg.de>
2024-12-15 01:18:43 +0100

    [Tests] Skip if tests are running online

--- tests/read/image/test_openslide.py
+++ tests/read/image/test_openslide.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 import openslide
 import pytest
@@ -5,6 +7,7 @@ import pytest
 from dvpio.read.image import read_openslide
 
 
+@pytest.mark.skipif(sys.platform != "darwin", reason="Tests fail online due to limited resources")
 @pytest.mark.parametrize(
     ("dataset", "xmin", "ymin", "xmax", "ymax"),
     [
