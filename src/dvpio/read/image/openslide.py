1253a7084adcfaf21623c15b1d3fa66744a75733
Lucas Diedrich <diedrich@biochem.mpg.de>
2024-12-16 18:46:00 +0100

    [Fix] Changed logic. Previously,   a nested list numpy arrays was (accidentally) generated which is extremely memory intensive and not fully parallelized, now, dask arrays are immediately generated after reading in the image, which reduces memory burden and allows dask to use lazy loading

--- src/dvpio/read/image/openslide.py
+++ src/dvpio/read/image/openslide.py
@@ -1,13 +1,12 @@
 """Reader for Whole Slide Images"""
 
-import dask.array as da
 import numpy as np
 import openslide
 from dask import delayed
 from numpy.typing import NDArray
 from spatialdata.models import Image2DModel
 
-from ._utils import _assemble_delayed, _chunk_factory, _create_tiles
+from ._utils import _assemble, _chunk_factory, _create_tiles
 
 
 @delayed
@@ -101,11 +100,11 @@ def read_openslide(path: str, chunk_size: tuple[int, int] = (10000, 10000), pyra
     # Define coordinates for chunkwise loading of the slide
     chunk_coords = _create_tiles(dimensions=dimensions, tile_size=chunk_size, min_coordinates=(0, 0))
 
-    chunks = _chunk_factory(_get_img, slide=slide, coords=chunk_coords, level=0)
+    # Load chunkwise (parallelized with dask.delayed)
+    chunks = _chunk_factory(_get_img, slide=slide, coords=chunk_coords, n_channel=4, level=0)
 
     # Assemble into a single dask array
-    array_ = _assemble_delayed(chunks)
-    array = da.from_delayed(array_, shape=(4, *dimensions[::-1]), dtype=np.uint8).rechunk()
+    array = _assemble(chunks)
 
     return Image2DModel.parse(
         array,
