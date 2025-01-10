dedac892c855fecd5cfe3ed1dbfbc088c89cf827
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-01-10 18:55:25 +0100

    [Fix] Updated DVP writer to accomodate points model, validate input

--- src/dvpio/write/lmd_writer.py
+++ src/dvpio/write/lmd_writer.py
@@ -1,19 +1,15 @@
 import os
 
-import geopandas as gpd
 import lmd.lib as pylmd
-import numpy as np
 import spatialdata as sd
 
 
 def write_lmd(
     path: str,
-    annotation: gpd.GeoDataFrame | sd.models.ShapesModel,
-    calibration_points: sd.models.PointsModel | gpd.GeoDataFrame,
-    annotation_geometry_column: str = "geometry",
+    annotation: sd.models.ShapesModel,
+    calibration_points: sd.models.PointsModel,
     annotation_name_column: str | None = None,
     annotation_well_column: str | None = None,
-    calibration_points_geometry_column: str = "geometry",
     overwrite: bool = True,
 ) -> None:
     """Write cell annotations to Leica-compatible .xml file
@@ -36,7 +32,43 @@ def write_lmd(
         Will be stored in as the tag of the Shape in the xml file.
     calibration_points_geometry_column
         Column name of Points in `calibration_points` dataframe
+
+    Returns
+    -------
+    Saves to path
+
+    Example
+    -------
+    .. code-block:: python
+        from spatialdata.models import ShapesModel, PointsModel
+        from tempfile import mkdtemp
+        from dvpio.write import write_lmd
+
+        gdf = ShapesModel.parse(
+            gpd.GeoDataFrame(
+                data={"name": ["001"], "well": ["A1"]}, geometry=[shapely.Polygon([[0, 0], [0, 1], [1, 0], [0, 0]])]
+            )
+        )
+
+        calibration_points_image = PointsModel.parse(np.array([[15, 1015], [15, 205], [1015, 15]]))
+
+        path = os.path.join(mkdtemp(), "test.xml")
+
+        write_lmd(
+            path=path,
+            annotation=gdf,
+            calibration_points=calibration_points,
+            annotation_geometry_column="geometry",
+            annotation_name_column=annotation_name_column,
+            annotation_well_column=annotation_well_column,
+            overwrite=True,
+        )
+
     """
+    # Validate input
+    sd.models.ShapesModel.validate(annotation)
+    sd.models.PointsModel.validate(calibration_points)
+
     if len(calibration_points) < 3:
         raise ValueError(f"There must be at least 3 points, currently only {len(calibration_points)}")
 
@@ -44,9 +76,7 @@ def write_lmd(
         raise ValueError(f"Path {path} exists and overwrite is False")
 
     # Convert calibration points dataframe to (N, 2) array for pylmd
-    calibration_points = np.array(
-        calibration_points[calibration_points_geometry_column].apply(lambda point: [point.x, point.y]).tolist()
-    )
+    calibration_points = calibration_points.to_dask_array().compute()
 
     # Create pylmd collection
     collection = pylmd.Collection(calibration_points=calibration_points)
@@ -54,7 +84,7 @@ def write_lmd(
     # Load annotation and optional columns
     collection.load_geopandas(
         annotation,
-        geometry_column=annotation_geometry_column,
+        geometry_column="geometry",
         name_column=annotation_name_column,
         well_column=annotation_well_column,
     )
