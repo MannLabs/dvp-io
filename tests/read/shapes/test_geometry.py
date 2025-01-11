e5d3c78776dca6224b85bf910961e13bc4347b59
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-01-11 18:53:02 +0100

    [Tests] Refactor tests to test for affine transformation instead of rotation/scale+translation

--- tests/read/shapes/test_geometry.py
+++ tests/read/shapes/test_geometry.py
@@ -3,62 +3,59 @@ import numpy as np
 import pytest
 from numpy.typing import NDArray
 from shapely import Polygon
-from spatialdata.models import PointsModel
+from spatialdata.models import PointsModel, ShapesModel
 
-from dvpio.read.shapes.geometry import apply_affine_transformation, compute_affine_transformation, transform_shapes
+from dvpio.read.shapes.geometry import (
+    _apply_affine_transformation,
+    compute_affine_transformation,
+    transform_shapes,
+)
 
 test_cases = [
     # Scale
     (
         np.array([[0, 0], [1, 0], [0, 1]]),
         np.array([[0, 0], [2, 0], [0, 2]]),
-        np.array([[2, 0], [0, 2]]),
-        np.array([[0, 0]]),
+        np.array([[2, 0, 0], [0, 2, 0], [0, 0, 1]]),
     ),
     # Translation
     (
         np.array([[0, 0], [1, 0], [0, 1]]),
         np.array([[1, 1], [2, 1], [1, 2]]),
-        np.array([[1, 0], [0, 1]]),
-        np.array([[1, 1]]),
+        np.array([[1, 0, 0], [0, 1, 0], [1, 1, 1]]),
     ),
     # Rotation
     (
         np.array([[0, 0], [1, 0], [0, 1]]),
         np.array([[0, 0], [0, -1], [1, 0]]),
-        np.array([[0, -1], [1, 0]]),
-        np.array([[0, 0]]),
+        np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]]),
     ),
     # Rotate (-90degrees), scale (x2), translate (1,1)
     (
         np.array([[0, 0], [1, 0], [0, 1]]),
         np.array([[1, 1], [1, 3], [-1, 1]]),
-        np.array([[0, 2], [-2, 0]]),
-        np.array([[1, 1]]),
+        np.array([[0, 2, 0], [-2, 0, 0], [1, 1, 1]]),
     ),
 ]
 
 
-@pytest.mark.parametrize(["query", "reference", "transformation", "translation"], test_cases)
+@pytest.mark.parametrize(["query", "reference", "affine_transformation"], test_cases)
 def test_compute_affine_transformation(
     query: NDArray[np.float64],
     reference: NDArray[np.int64],
-    transformation: NDArray[np.int64],
-    translation: NDArray[np.int64],
+    affine_transformation: NDArray[np.int64],
 ) -> None:
-    inferred_transformation, inferred_translation = compute_affine_transformation(query, reference, precision=3)
-    assert np.isclose(inferred_transformation, transformation, rtol=0.001).all()
-    assert np.isclose(inferred_translation, translation, rtol=0.001).all()
+    inferred_transformation = compute_affine_transformation(query, reference, precision=3)
+    assert np.isclose(inferred_transformation, affine_transformation, rtol=0.001).all()
 
 
-@pytest.mark.parametrize(["query", "reference", "transformation", "translation"], test_cases)
+@pytest.mark.parametrize(["query", "reference", "affine_transformation"], test_cases)
 def test_apply_affine_transformation(
     query: NDArray[np.float64],
-    reference: NDArray[np.int64],
-    transformation: NDArray[np.int64],
-    translation: NDArray[np.int64],
+    reference: NDArray[np.float64],
+    affine_transformation: NDArray[np.float64],
 ) -> None:
-    target = apply_affine_transformation(query, transformation, translation)
+    target = _apply_affine_transformation(query, affine_transformation)
     assert np.isclose(target, reference, rtol=0.001).all()
 
 
@@ -66,7 +63,7 @@ def test_transform_shapes() -> None:
     # Create data
     calibration_points = PointsModel.parse(np.array([[0, 0], [1, 0], [0, 1]]))
     shape = Polygon([[0, 0], [1, 1], [0, 1]])
-    shapes = gpd.GeoDataFrame(geometry=[shape] * 10)
+    shapes = ShapesModel.parse(gpd.GeoDataFrame(geometry=[shape] * 10))
 
     # Transform
     transformed_shapes = transform_shapes(
