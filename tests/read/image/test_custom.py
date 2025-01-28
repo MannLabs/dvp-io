3926887f824b41018ee641660e291ff42e021bc5
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-01-28 15:57:11 +0100

    [Tests] Fix tests. Temporary solution

--- tests/read/image/test_custom.py
+++ tests/read/image/test_custom.py
@@ -6,7 +6,8 @@ from dvpio.read.image import read_custom
 
 @pytest.mark.parametrize(["filename"], [["./data/blobs/blobs/images/binary-blobs.tiff"]])
 def test_custom(filename: str) -> None:
-    img = read_custom(filename, dims=("c", "y", "x"))
+    # TODO Create a more elegant solution
+    img = read_custom(filename, imread=lambda path: tiffread(path).squeeze(), dims=("c", "y", "x"))
 
     img_groundtruth = tiffread(filename)
 
