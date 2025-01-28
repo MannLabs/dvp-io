034bd383aeadd3e90ecf16c7c43bdc01021c8556
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-01-28 15:59:42 +0100

    [Tests] Test globstrings, simple test

--- tests/read/image/test_custom.py
+++ tests/read/image/test_custom.py
@@ -4,7 +4,9 @@ from tifffile import imread as tiffread
 from dvpio.read.image import read_custom
 
 
-@pytest.mark.parametrize(["filename"], [["./data/blobs/blobs/images/binary-blobs.tiff"]])
+@pytest.mark.parametrize(
+    ["filename"], [["./data/blobs/blobs/images/binary-blobs.tiff"], ["./data/blobs/blobs/images/binary-blobs*.tiff"]]
+)
 def test_custom(filename: str) -> None:
     # TODO Create a more elegant solution
     img = read_custom(filename, imread=lambda path: tiffread(path).squeeze(), dims=("c", "y", "x"))
