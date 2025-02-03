eb0f519291ecc24cdf4c63b70f7053f61bd51757
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-02-03 09:47:31 +0100

    Added experimental warnings

---
+++ src/dvpio/_utils.py
@@ -0,0 +1,25 @@
+import functools
+import warnings
+
+
+def experimental_docs(func):
+    """Decorator to mark a function as experimental in the docstring."""
+    func.__doc__ = (
+        func.__doc__ or ""
+    ) + "\n\n    **Warning:** This function is experimental and may change in future versions."
+    return func
+
+
+def experimental_log(func):
+    """Decorator to mark a function as experimental with a warning log."""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        warnings.warn(
+            f"Function {func.__name__} is experimental and may change in future versions.",
+            category=UserWarning,
+            stacklevel=2,
+        )
+        return func(*args, **kwargs)
+
+    return wrapper
