44383ec4fe3586ab4c0e248407ac9dc9d6ad1de5
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-02-24 19:53:23 +0100

    [Tests] Update tests with new argument names in functions

--- tests/read/image/test_utils.py
+++ tests/read/image/test_utils.py
@@ -10,7 +10,7 @@ from dvpio.read.image._utils import _compute_chunks, _read_chunks
 
 
 @pytest.mark.parametrize(
-    ("dimensions", "tile_size", "min_coordinates", "result"),
+    ("dimensions", "chunk_size", "min_coordinates", "result"),
     [
         # Regular grid 2x2
         (
@@ -37,17 +37,17 @@ from dvpio.read.image._utils import _compute_chunks, _read_chunks
 )
 def test_compute_chunks(
     dimensions: tuple[int, int],
-    tile_size: tuple[int, int],
+    chunk_size: tuple[int, int],
     min_coordinates: tuple[int, int],
     result: NDArray,
 ) -> None:
-    tiles = _compute_chunks(dimensions=dimensions, tile_size=tile_size, min_coordinates=min_coordinates)
+    tiles = _compute_chunks(dimensions=dimensions, chunk_size=chunk_size, min_coordinates=min_coordinates)
 
     assert (tiles == result).all()
 
 
 @pytest.mark.parametrize(
-    ("dimensions", "tile_size", "min_coordinates"),
+    ("dimensions", "chunk_size", "min_coordinates"),
     [
         # Regular grid 2x2
         (
@@ -71,7 +71,7 @@ def test_compute_chunks(
 )
 def test_read_chunks(
     dimensions: tuple[int, int],
-    tile_size: tuple[int, int],
+    chunk_size: tuple[int, int],
     min_coordinates: tuple[int, int],
 ) -> None:
     """Test if tiles can be assembled to dask array"""
@@ -81,7 +81,7 @@ def test_read_chunks(
         """Create arrays in shape of tiles"""
         return da.zeros(shape=size)
 
-    coords = _compute_chunks(dimensions=dimensions, tile_size=tile_size, min_coordinates=min_coordinates)
+    coords = _compute_chunks(dimensions=dimensions, chunk_size=chunk_size, min_coordinates=min_coordinates)
 
     tiles_ = _read_chunks(func, slide=None, coords=coords, n_channel=1, dtype=np.uint8)
     tiles = da.block(tiles_)
@@ -98,7 +98,7 @@ def test_read_chunks_dtype(dtype) -> None:
         """Create arrays in shape of tiles"""
         return da.zeros(shape=size)
 
-    coords = _compute_chunks(dimensions=(2, 2), tile_size=(1, 1), min_coordinates=(0, 0))
+    coords = _compute_chunks(dimensions=(2, 2), chunk_size=(1, 1), min_coordinates=(0, 0))
 
     tiles_ = _read_chunks(func, slide=None, coords=coords, n_channel=1, dtype=dtype)
     tiles = da.block(tiles_)
