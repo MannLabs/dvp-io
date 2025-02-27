e25001af1ed1712773ba5ae56e6185a61c96fbbf
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-02-27 13:49:22 +0100

    [Refactor] Change NotImplementedError to ellipsis in line with official ABC documentation https://docs.python.org/3/library/abc.html

--- src/dvpio/read/image/_metadata.py
+++ src/dvpio/read/image/_metadata.py
@@ -27,43 +27,43 @@ class ImageMetadata(BaseModel, ABC):
         This value does not consider the magnifications by additional optical elements
         in the specific microscopy setup
         """
-        raise NotImplementedError
+        ...
 
     @property
     @abstractmethod
     def mpp_x(self) -> float:
         """Resolution of the image in meters per pixel along x-axis"""
-        raise NotImplementedError
+        ...
 
     @property
     @abstractmethod
     def mpp_y(self) -> float:
         """Resolution of the image in meters per pixel along y-axis"""
-        raise NotImplementedError
+        ...
 
     @property
     @abstractmethod
     def mpp_z(self) -> float:
         """Resolution of the image in meters per pixel along z-axis"""
-        raise NotImplementedError
+        ...
 
     @property
     @abstractmethod
     def channel_names(self) -> list[str] | None:
         """Names of the microscopy channels"""
-        raise NotImplementedError
+        ...
 
     @property
     @abstractmethod
     def image_type(self) -> str:
         """Indicator of the original image format/microscopy vendor"""
-        raise NotImplementedError
+        ...
 
     @classmethod
     @abstractmethod
     def from_file(path: str):
         """Load metadata from microscopy image in path"""
-        raise NotImplementedError
+        ...
 
 
 class CZIImageMetadata(ImageMetadata):
