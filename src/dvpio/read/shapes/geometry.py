70625bf71b30e2a33043731acc2ca521748851d5
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-04-13 17:57:16 +0200

    [REF] Refactor geometry to leverage single skimage API

--- src/dvpio/read/shapes/geometry.py
+++ src/dvpio/read/shapes/geometry.py
@@ -1,10 +1,15 @@
+from typing import Literal
+
 import numpy as np
 from numpy.typing import NDArray
 from skimage.transform import estimate_transform
 
 
 def compute_affine_transformation(
-    query_points: NDArray[np.float64], reference_points: NDArray[np.float64], precision: int | None = None
+    query_points: NDArray[np.float64],
+    reference_points: NDArray[np.float64],
+    transformation_type: Literal["similarity", "affine", "euclidean"],
+    precision: int | None = None,
 ) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
     """Computes the affine transformation mapping query_points to reference_points.
 
@@ -17,6 +22,15 @@ def compute_affine_transformation(
         An (N, 2) array of points in the query coordinate system.
     reference_points
         An (N, 2) array of corresponding points in the reference coordinate system.
+    transformation_type
+        - affine
+            Full affine transformation (scaling, rotation/reflexion, translation, shearing)
+        - similarity
+            Similarity transformation. Compared to an affine transformation, a similarity transformation constraints
+            the solution space to scaling, rotations, reflections, and translations, i.e. angles of shapes are retained.
+            precision
+        - euclidean (Rigid transform)
+            Only translation and rotation are allowed
     precision
         Rounding of affine transformation matrix
 
@@ -33,51 +47,7 @@ def compute_affine_transformation(
     if query_points.shape[0] < 3:
         raise ValueError("At least three points are required to compute the transformation.")
 
-    affine_matrix = estimate_transform(ttype="affine", src=query_points, dst=reference_points)
-
-    if precision is not None:
-        affine_matrix = np.around(affine_matrix, precision)
-
-    return affine_matrix.T
-
-
-def compute_similarity_transformation(
-    query_points: NDArray[np.float64], reference_points: NDArray[np.float64], precision: int | None = None
-):
-    """Compute the similarity transformation that maps query_points to reference_points
-
-    Compared to an affine transformation, a similarity transformation constraints the solution space
-    to scaling, rotations, reflections, and translations, i.e. angles of shapes are retained.
-
-    .. math::
-            Aq = r
-
-    Parameters
-    ----------
-    query_points
-        An (N, 2) array of points in the query coordinate system.
-    reference_points
-        An (N, 2) array of corresponding points in the reference coordinate system.
-    precision
-        Rounding of affine transformation matrix
-
-    Returns
-    -------
-    similarity_transformation
-        Similarity transformation as (3 x 3) matrix
-
-    References
-    ----------
-    Least-squares estimation of transformation parameters between two point patterns, Shinji Umeyama, PAMI 1991, DOI: 10.1109/34.88573
-    """
-    if query_points.shape != reference_points.shape:
-        raise ValueError("Point sets must have the same shape.")
-    if query_points.shape[1] != 2:
-        raise ValueError("Points must be 2D.")
-    if query_points.shape[0] < 3:
-        raise ValueError("At least three points are required to compute the transformation.")
-
-    affine_matrix = estimate_transform(ttype="similarity", src=query_points, dst=reference_points)
+    affine_matrix = estimate_transform(ttype=transformation_type, src=query_points, dst=reference_points)
 
     if precision is not None:
         affine_matrix = np.around(affine_matrix, precision)
