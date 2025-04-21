d38c319a299af5411b51d1d9831640a0f7ff66f6
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-04-22 01:30:40 +0200

    [Test] Add test that captures bug.

--- tests/read/image/test_openslide.py
+++ tests/read/image/test_openslide.py
@@ -3,6 +3,23 @@ import openslide
 import pytest
 
 from dvpio.read.image import read_openslide
+from dvpio.read.image.openslide import _get_img
+
+
+@pytest.mark.parametrize(
+    ("dataset", "xmin", "ymin", "width", "height"),
+    [
+        # Asymmetric
+        ("./data/openslide-mirax/Mirax2.2-4-PNG.mrxs", 0, 0, 500, 1000),
+    ],
+)
+def test_get_image_openslide(dataset, xmin: int, ymin: int, width: int, height: int) -> None:
+    slide = openslide.OpenSlide(dataset)
+    ground_truth_shape = (4, height, width)
+
+    img = _get_img(slide, x0=xmin, y0=ymin, width=width, height=height, level=0)
+
+    assert all(img_dim == ref_dim for img_dim, ref_dim in zip(img.shape, ground_truth_shape, strict=True))
 
 
 # @pytest.mark.skipif(sys.platform != "darwin", reason="Tests fail online due to limited resources")
