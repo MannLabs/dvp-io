b809262178f106706b6891750bc92e7898ebe70e
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-08-29 07:34:26 +0200

    [Feature] Add deprecated decorator

--- src/dvpio/_utils.py
+++ src/dvpio/_utils.py
@@ -31,3 +31,45 @@ def experimental_log(func):
         return func(*args, **kwargs)
 
     return wrapper
+
+
+def deprecated_docs(func):
+    """Decorator to mark a function as deprecated in the docstring."""
+    func.__doc__ = f"""**Warning: This function is deprecated and will be removed in the next minor release**\n\n
+    {func.__doc__ or ""}"""
+    return func
+
+
+def deprecated_log(message=None):
+    """Decorator to mark a function as deprecated with a warning log.
+
+    Parameters
+    ----------
+    message
+        Optional custom deprecation message. If not provided, uses default format.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if message is None:
+                warning_message = f"Function {func.__name__} is deprecated and will be removed in future versions."
+            else:
+                warning_message = message
+
+            warnings.warn(
+                warning_message,
+                category=UserWarning,
+                stacklevel=2,
+            )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    # Handle both @deprecated_log and @deprecated_log() usage
+    if callable(message):
+        func = message
+        message = None
+        return decorator(func)
+
+    return decorator
